647b2578c4932f1bb6c914f78760221307455fd6
nickrdandrea <nickrdandrea@gmail.com>
2017-01-25 16:12:30 -0500

    Originally cloned without forking. I forked then cloned and copied the files into the new repo.

--- crm.rb
+++ crm.rb
@@ -1,3 +1,23 @@
 # Implement the new web-based CRM here.
 # Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
 # You'll have to implement it from scratch.
+require_relative 'contact'
+require 'sinatra'
+
+get '/' do
+  @crm_app_name = "Nick's CRM"
+  erb :index
+end
+
+get "/contacts" do
+  erb :contacts
+end
+
+get "/contacts/new" do
+  erb :new_contact
+end
+
+post '/contacts' do
+  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+  redirect to('/contacts')
+end
